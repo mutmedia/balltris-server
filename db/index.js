0712a813cf07d3ba4609d46c9a71fd47125a6969
mutmedia <gustavo.ceci95@gmail.com>
2021-01-07 17:32:15 -0500

    oops

--- db/index.js
+++ db/index.js
@@ -1,19 +1,19 @@
-const MongoClient = require('mongodb').MongoClient;
-const config = require('config');
-const Model = require('./model');
+const MongoClient = require('mongodb').MongoClient
+const config = require('config')
+const Model = require('./model')
 const uri = config.db.url
 
-const db;
+let db
 
 class Db {
-  async connect() {
-    if(!db) {
-      db = new MongoClient(uri, { useNewUrlParser: true });
-      this.User = new Model(db, 'users');
-      this.Game = new Model(db, 'games');
-      db.createIndex('games', {userid: 1, game: 1}, {unique: true});
+  async connect () {
+    if (!db) {
+      db = new MongoClient(uri, { useNewUrlParser: true })
+      this.User = new Model(db, 'users')
+      this.Game = new Model(db, 'games')
+      db.createIndex('games', { userid: 1, game: 1 }, { unique: true })
     }
   }
 };
 
-module.exports = new Db();
+module.exports = new Db()
