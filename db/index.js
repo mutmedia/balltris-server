95ef57402275d391e41193bac8b31cac0dc7840c
mutmedia <gustavo.ceci95@gmail.com>
2021-01-07 17:31:04 -0500

    small update

--- db/index.js
+++ db/index.js
@@ -1,13 +1,14 @@
 const MongoClient = require('mongodb').MongoClient;
 const config = require('config');
 const Model = require('./model');
+const uri = config.db.url
 
-let db;
+const db;
 
 class Db {
   async connect() {
     if(!db) {
-      db = await MongoClient.connect(config.db.url);
+      db = new MongoClient(uri, { useNewUrlParser: true });
       this.User = new Model(db, 'users');
       this.Game = new Model(db, 'games');
       db.createIndex('games', {userid: 1, game: 1}, {unique: true});
